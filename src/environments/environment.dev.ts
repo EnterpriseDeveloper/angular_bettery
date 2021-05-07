f8a908320c12b13772bd952a7e933ee7267d7794
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-05-07 22:26:55 +0300

    typo

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,8 +4,7 @@ export const environment = {
   defaultLocale: 'en',
   apiUrl: 'http://localhost:80',
   biconomy: "iwIgyW3sM.12ac582c-bd06-4289-8d48-47ef552af03f",
-  // maticUrl: "https://rpc-mumbai.maticvigil.com/v1/0a4eb295e6abcd89e03e21b86b4e15a59fb1a8ab",
-  maticUrl: "https://matic-mainnet-archive-rpc.bwarelabs.com",
+  maticUrl: "https://rpc-mumbai.maticvigil.com/v1/0a4eb295e6abcd89e03e21b86b4e15a59fb1a8ab",
   maticId: 80001,
   etherId: 5,
   torusHost: "goerli"
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,8 @@ export const environment = {
   defaultLocale: 'en',
   apiUrl: 'https://api.bettery.io',
   biconomy: "qhib4zfV0.2244c10d-a20e-4cdc-b8fd-3080905f7444",
-  maticUrl: "https://rpc-mainnet.maticvigil.com/v1/0a4eb295e6abcd89e03e21b86b4e15a59fb1a8ab",
+  //maticUrl: "https://rpc-mainnet.maticvigil.com/v1/0a4eb295e6abcd89e03e21b86b4e15a59fb1a8ab",
+  maticUrl: "https://matic-mainnet-archive-rpc.bwarelabs.com",
   maticId: 137,
   etherId: 1,
   torusHost: "mainnet"
