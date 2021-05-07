45d633d34bac4d911005d79bfe63675a49da3aeb
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-05-07 22:09:08 +0300

    changed Matic provider

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   defaultLocale: 'en',
   apiUrl: 'https://api.bettery.io',
   biconomy: "qhib4zfV0.2244c10d-a20e-4cdc-b8fd-3080905f7444",
-  maticUrl: "https://rpc-mainnet.maticvigil.com",
+  maticUrl: "https://rpc-mainnet.maticvigil.com/v1/0a4eb295e6abcd89e03e21b86b4e15a59fb1a8ab",
   maticId: 137,
   etherId: 1,
   torusHost: "mainnet"
