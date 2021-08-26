c9d0d4f59d850e0af0281232f8ade71552494171
ANDRYXA <andriysivik5@gmail.com>
2021-08-26 18:32:59 +0300

    mobile version

--- src/app/components/user/my-payouts/my-payouts.component.ts
+++ src/app/components/user/my-payouts/my-payouts.component.ts
@@ -1,16 +1,20 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {PostService} from "../../../services/post.service";
-import {Payouts} from "../../../models/Payouts.model";
-import {Subscription} from "rxjs";
+import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
+import {PostService} from '../../../services/post.service';
+import {Payouts} from '../../../models/Payouts.model';
+import {Subscription} from 'rxjs';
+import {PayoutEvent} from '../../../models/PayoutEvent.model';
 
 @Component({
   selector: 'app-my-payouts',
   templateUrl: './my-payouts.component.html',
   styleUrls: ['./my-payouts.component.sass']
 })
-export class MyPayoutsComponent implements OnInit , OnDestroy{
+export class MyPayoutsComponent implements OnInit, OnDestroy, OnChanges {
   payouts: Payouts;
   payoutsSub: Subscription;
+  currentPage = 1;
+  @Input() userData;
+
 
   constructor(private postService: PostService) {
   }
@@ -19,16 +23,48 @@ export class MyPayoutsComponent implements OnInit , OnDestroy{
     this.getPayouts();
   }
 
-  getPayouts() {
-   this.payoutsSub = this.postService.post('user/ref_list', {
-      from: 0,
-      to: 5
-    }).subscribe((value: Payouts) => {
-      this.payouts = value;
-      console.log(this.payouts);
+  getPayouts(from= 0, to = 5) {
+    if (this.userData) {
+      this.payoutsSub = this.postService.post('user/ref_list', {
+        from,
+        to
+      }).subscribe((value: Payouts) => {
+        this.payouts = value;
+        // value.data.forEach(value => {
+        //   for (let i = 0 ; i<5;i++){
+        //     this.payouts.data.push(value);
+        //   }
+        // })
+
+        console.log(this.payouts);
+      });
+    }
+  }
+
+  myReferralsCalculate(payout: PayoutEvent) {
+    let myReferrals = 0;
+    payout.parcipiantsAnswer.forEach(value => {
+      value.byMyRef && myReferrals++;
     });
+    return myReferrals;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.userData.currentValue !== undefined) {
+      if (changes.userData.previousValue?._id !== changes.userData.currentValue?._id) {
+        this.payouts = undefined;
+        this.getPayouts();
+      }
+      this.getPayouts();
+    }
+  }
+  goToPage(i){
+    this.currentPage = i ;
+    this.getPayouts(i * 5, (i * 5) - 5);
   }
   ngOnDestroy() {
-    // this.payoutsSub.unsubscribe();
+    if (this.payoutsSub) {
+      this.payoutsSub.unsubscribe();
+    }
   }
 }
