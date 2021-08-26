1b724573ac7825a4efa73ea30d3c3b331cbb7738
ANDRYXA <andriysivik5@gmail.com>
2021-08-26 11:22:17 +0300

    initial commit

---
+++ src/app/components/user/my-payouts/my-payouts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-payouts',
+  templateUrl: './my-payouts.component.html',
+  styleUrls: ['./my-payouts.component.sass']
+})
+export class MyPayoutsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
