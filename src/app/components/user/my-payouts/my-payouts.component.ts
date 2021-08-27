a9440fbfa3fd64618dc01630de95044901d843f4
ANDRYXA <andriysivik5@gmail.com>
2021-08-27 16:43:44 +0300

    mobile version, pagination test example

--- src/app/components/user/my-payouts/my-payouts.component.ts
+++ src/app/components/user/my-payouts/my-payouts.component.ts
@@ -13,6 +13,8 @@ export class MyPayoutsComponent implements OnInit, OnDestroy, OnChanges {
   payouts: Payouts;
   payoutsSub: Subscription;
   currentPage = 1;
+  pagesArr = [];
+  pagesToShow: [];
   @Input() userData;
 
 
@@ -21,9 +23,10 @@ export class MyPayoutsComponent implements OnInit, OnDestroy, OnChanges {
 
   ngOnInit(): void {
     this.getPayouts();
+
   }
 
-  getPayouts(from= 0, to = 5) {
+  getPayouts(from = 0, to = 5) {
     if (this.userData) {
       this.payoutsSub = this.postService.post('user/ref_list', {
         from,
@@ -35,7 +38,8 @@ export class MyPayoutsComponent implements OnInit, OnDestroy, OnChanges {
         //     this.payouts.data.push(value);
         //   }
         // })
-
+        this.pagesArr = [];
+        this.calcPages();
         console.log(this.payouts);
       });
     }
@@ -56,12 +60,74 @@ export class MyPayoutsComponent implements OnInit, OnDestroy, OnChanges {
         this.getPayouts();
       }
       this.getPayouts();
+
     }
   }
-  goToPage(i){
-    this.currentPage = i ;
-    this.getPayouts(i * 5, (i * 5) - 5);
+
+  goToPage(i) {
+    if (i <= 1){
+      i = 1;
+    }
+    if (i <= this.pagesArr.length){
+
+      this.getPayouts((i * 5) - 5, i * 5);
+    }
+    this.pagesArr = [];
+    this.calcPages(i);
   }
+
+  allPages() {
+    if (this.payouts) {
+      return Math.ceil(this.payouts.eventsAmount / 5);
+      //  return  10;
+    }
+    console.log(this.pagesToShow);
+  }
+
+  calcPages(i= 1 ) {
+    this.pagesToShow = [] ;
+
+    if (this.payouts) {
+      for (let i = 1; i <= this.allPages(); i++) {
+        this.pagesArr.push(i);
+      }
+      // for (let i = 1; i <= 10; i++ ){
+      //   this.pagesArr.push(i);
+      // }
+      if (this.pagesArr.length < 5) {
+        console.log(this.currentPage);
+        this.pagesToShow = this.pagesArr as [];
+        console.log(this.pagesToShow, 'pagestoshow');
+        return;
+      }
+      if (i <= 1) {
+        this.currentPage = 1;
+        this.pagesToShow = this.pagesArr as [];
+        return;
+      }
+      if (i >= this.pagesArr.length) {
+        this.currentPage = this.pagesArr.length;
+        this.pagesToShow = [...this.pagesArr.slice(i - 4, i)] as [];
+        return;
+      }
+      this.currentPage = i;
+
+      if (this.pagesArr.length > 5 && this.currentPage !== 1 && this.currentPage !== this.pagesArr.length) {
+        this.pagesToShow = [...this.pagesArr.slice(i - 2, i + 2)] as [];
+      }
+      if (this.currentPage === this.pagesArr.length - 3) {
+        this.pagesToShow = [...this.pagesArr.slice(i - 4, i)] as [];
+      }
+      console.log(this.pagesToShow, 'pagestoshow');
+      if (this.pagesArr.length > 5 ) {
+        this.pagesToShow = this.pagesArr.slice(0, 4) as [];
+
+        return this.pagesToShow;
+      }
+    }
+
+  }
+
   ngOnDestroy() {
     if (this.payoutsSub) {
       this.payoutsSub.unsubscribe();
